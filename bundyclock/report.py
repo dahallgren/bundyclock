89e13a7e5bfa3626014b6e4cd28ac925ff150d47
Dan Hallgren <dan.hallgren@gmail.com>
2018-11-04 09:11:46 +0100

    refactoring of render module

--- bundyclock/report.py
+++ bundyclock/report.py
@@ -7,12 +7,12 @@ import jinja2
 
 
 # jinja2 filters
-def lunch(total_time, total_lunch_minutes):
+def subtract_minutes(time_hms, minute_subtrahend):
     """"
     jinja2 filter that subtracts given minutes from time in HH:MM:SS format
     """
-    (h, m, s) = map(int, total_time.split(':'))
-    seconds = h*3600 + m*60 + s - total_lunch_minutes*60
+    (h, m, s) = map(int, time_hms.split(':'))
+    seconds = h * 3600 + m * 60 + s - minute_subtrahend * 60
 
     h, s = divmod(seconds, 3600)
     m, s = divmod(s, 60)
@@ -22,17 +22,18 @@ def lunch(total_time, total_lunch_minutes):
 
 def render(some_date, ledger, template):
     year_month = guess_date(some_date).strftime('%Y-%m')
-    template_env = jinja2.Environment(loader=jinja2.PackageLoader('bundyclock', 'templates'))
-    template_env.filters['lunch'] = lunch
     start_date = re.sub(r'(\d{4})-(\d{2}).*', r'\1-\2-01', year_month)
     last_day_of_month = monthrange(*map(int, year_month.split('-')[:2]))[1]
     end_date = re.sub(r'(\d{4})-(\d{2}).*', r'\1-\2-{}', year_month) \
         .format(last_day_of_month)
 
     try:
+        template_env = jinja2.Environment(loader=jinja2.PackageLoader('bundyclock', 'templates'))
+        template_env.filters['lunch'] = subtract_minutes
         template = template_env.get_template(template)
     except jinja2.exceptions.TemplateNotFound:
         template_env = jinja2.Environment(loader=jinja2.FileSystemLoader(searchpath="./"))
+        template_env.filters['lunch'] = subtract_minutes
         template = template_env.get_template(template)
 
     context = dict(
